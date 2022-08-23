38148e5e185fc11fe6bf52de235d48dcc51597cd
Kim Minjoo <roses16.dev@gmail.com>
2022-08-23 19:17:04 +0900

    movie recommend

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
 from pymongo import MongoClient
 app = Flask(__name__)
 
 import certifi, requests, os
-client = MongoClient(os.environ.get("MONGO"),tlsCAFile=certifi.where())
+client = MongoClient('mongodb+srv://Sorting_Hat_Read:Sorting_Hat_Read@cluster0.amhacid.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=certifi.where())
 db = client.Sorting_Hat_Dev
 
 # movies_list = list(db.movies.find({}, {'_id': False}))
@@ -16,6 +16,29 @@ def home():
 @app.route('/recommend')
 def recommend():
     return render_template('recommend.html')
+
+@app.route('/recommend/calc', methods=["POST"])
+def recommend_calc():
+    movies_list = list(db.movies.find({'showing': 1}, {'_id': False}).sort('reviewer_star', -1))
+    rate_receive = int(request.form['give_rate'])
+    genre_receive = request.form.getlist('give_genre[]')
+
+    print(genre_receive, rate_receive)
+
+    for movie in movies_list:
+        print(movie)
+        if movie['age'] == '청소년 관람불가' and rate_receive < 3:
+            continue
+        elif movie['age'] == '15세 관람가' and rate_receive < 2:
+            continue
+        elif movie['age'] == '12세 관람가' and rate_receive < 1:
+            continue
+        else:
+            for m_genre in movie['genre']:
+                if genre_receive.count(m_genre) >= 1:
+                    print(movie['title'])
+                    return render_template('result.html', r_title=movie['title'])
+            return render_template('noresult.html')
 
 @app.route('/schedule')
 def scadule():
