bde6b1c8722851ca5c3a43b40ea0b484a36db0e6
Puri12 <tkdqja9812@gmail.com>
2022-08-24 22:10:00 +0900

    #40 상영예정작 @JMKiim 님 코드기반으로 상세페이지 기능 추가

--- static/modal.js
+++ static/modal.js
@@ -1,20 +1,10 @@
-//console.log('test');
 // Modal open & close
-const openButton = document.getElementsByClassName('DPimg');
-//const modal = document.querySelector('.modal');
+const openButton = document.getElementsByClassName('screen');
 const modal = document.getElementById('modal-container');
 const overlay = modal.querySelector('.modal_overlay');
 const closeBtn = modal.querySelector('.close_btn');
 
-/*
-const openModal = () => {
-  modal.classList.remove('hidden');
-};
-const closeModal = () => {
-  modal.classList.add('hidden');
-};*/
 
-//overlay.addEventListener('click', closeModal);
 closeBtn.addEventListener('click', function (e) {
   modal.classList.toggle('opaque');
 
@@ -33,9 +23,11 @@ overlay.addEventListener('click', function (e) {
   });
 });
 
-for (let i = 0; i < openButton.length; i++) {
-  openButton[i].addEventListener('click', function (e) {
-    modal.classList.toggle('opaque');
-    modal.classList.toggle('unstaged');
-  });
-}
+window.addEventListener("load", function(event) {
+  for (let i = 0; i < openButton.length; i++) {
+    openButton[i].addEventListener('click', function (e) {
+      modal.classList.toggle('opaque');
+      modal.classList.toggle('unstaged');
+    });
+  }
+})
